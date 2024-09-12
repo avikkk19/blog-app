953224f4c3c40488cf4602dc9b0cef358eb30c74
avikkk19 <avinashkamadri14@gmail.com>
2024-09-12 14:25:01 +0530

    removed env

--- api/server.js
+++ api/server.js
@@ -1,6 +1,16 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.eventNames.MANGO)
+  .then(() => {
+    console.log("mongo server is connected");
+  })
+  .catch(() => {
+    console.log("boss there is an error");
+  });
 const app = express();
 
-app.listen(3000,()=>{
-    console.log("sir server is running")
+app.listen(3000, () => {
+  console.log("sir mongodb is running");
 });
